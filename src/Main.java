fcd47382797fd52c5b5c6e8b9273aa2c768a852a
Alex <ccaassppeerr@list.ru>
2024-03-19 09:10:16 +0300

    hw is done!

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println(i);
         //Task 3
         System.out.println("Task 3");
-        for (int i = 2; i < 18; i+=2)
+        for (int i = 2; i < 18; i += 2)
             System.out.println(i);
         //Task 4
         System.out.println("Task 4");
@@ -19,21 +19,21 @@ public class Main {
             System.out.println(i);
         //Task 5
         System.out.println("Task 5");
-        for (int i = 1904; i < 2095; i+=4)
+        for (int i = 1904; i < 2095; i += 4)
             System.out.println(i);
         //Task 6
         System.out.println("Task 6");
-        for (int i = 7; i < 99; i+=7)
+        for (int i = 7; i < 99; i += 7)
             System.out.println(i);
         //Task 7
         System.out.println("Task 7");
-        for (int i = 1; i < 516; i*=2)
+        for (int i = 1; i < 516; i *= 2)
             System.out.println(i);
         //Task 8
         System.out.println("Task 8");
         int saving = 0;
         int savingChange = 29000;
-        for (int i = 1; i < 13; i++){
+        for (int i = 1; i < 13; i++) {
             saving += savingChange;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + saving + " рублей");
         }
@@ -41,7 +41,7 @@ public class Main {
         System.out.println("Task 9");
         double saving1 = 0;
         int savingChange1 = 29000;
-        for (int i = 1; i < 13; i++){
+        for (int i = 1; i < 13; i++) {
             saving1 += savingChange1;
             saving1 *= 1.12;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + (int) saving1 + " рублей");
@@ -49,6 +49,6 @@ public class Main {
         //Task 10
         System.out.println("Task 10");
         for (int i = 1; i < 11; i++)
-            System.out.println("2*" + i + "=" + 2*i);
+            System.out.println("2*" + i + "=" + 2 * i);
     }
 }
